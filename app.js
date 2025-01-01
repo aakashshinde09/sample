16c217480385ade5e971b31477a90c2e39c88b51
Akash Shinde <saakashs1999@gmail.com>
2025-01-01 17:13:23 +0530

    api added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello, World! This is a Node.js app deployed on ECS Fargate.');
 });
 
+// About Page
+app.get('/about', (req, res) => {
+  res.send('This is the about page. Here we share details about the application.');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
